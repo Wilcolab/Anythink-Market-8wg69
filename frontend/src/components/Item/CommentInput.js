c4eee66b2651c058e2ed90eb8c517cd67d30ef94
Divyansh Pingle <divyanshpingle900@gmail.com>
2025-06-12 17:12:57 +0000

    fix: Destructure `props` in `CommentInput` component

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -8,42 +8,41 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 // This component allows users to input comments on an item.
+function CommentInput({ currentUser, slug, onSubmit }) {
+  const [state, setState] = useState({ body: '' });
 
-function CommentInput({currentUser, slug}){
-  const [state, setState] = useState({body: ''});
-
-  async function createComment(e){
+  async function createComment(e) {
     e.preventDefault();
     const payload = await agent.Comments.create(slug, {
       body: state.body,
     });
     onSubmit(payload);
     setState({ body: '' });
-    };
-  
-    return (
-      <form className="card comment-form m-2" onSubmit={createComment}>
-        <div className="card-block">
-          <textarea
-            className="form-control"
-            placeholder="Write a comment..."
-            value={state.body}
-            onChange={e => setState({ body: e.target.value })}
-            rows="3"
-          ></textarea>
-        </div>
-        <div className="card-footer">
-          <img
-            src={currentUser.image}
-            className="user-pic mr-2"
-            alt={currentUser.username}
-          />
-          <button className="btn btn-sm btn-primary" type="submit">
-            Post Comment
-          </button>
-        </div>
-      </form>
-  )
+  }
+
+  return (
+    <form className="card comment-form m-2" onSubmit={createComment}>
+      <div className="card-block">
+        <textarea
+          className="form-control"
+          placeholder="Write a comment..."
+          value={state.body}
+          onChange={e => setState({ body: e.target.value })}
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={currentUser.image}
+          className="user-pic mr-2"
+          alt={currentUser.username}
+        />
+        <button className="btn btn-sm btn-primary" type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default connect(() => ({}), mapDispatchToProps)(CommentInput);
